fe3e77316d7006c652929bab1c66bddb45911512
Habib Ur Rehman <urrehmanhabib443@gmail.com>
2024-07-23 15:32:49 +0500

    create search api routes

--- api/routes/listing.routes.js
+++ api/routes/listing.routes.js
@@ -4,6 +4,7 @@ import {
     deleteListing,
     updateListing,
     getListing,
+    getAllListings,
 } from "../controller/createListing.controller.js";
 import { verifyToken } from "../middleware/auth.middleware.js";
 
@@ -13,4 +14,6 @@ router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
 router.post("/update/:id", verifyToken, updateListing);
 router.get("/get/:id", getListing);
+router.get("/get", getAllListings);
+
 export default router;
